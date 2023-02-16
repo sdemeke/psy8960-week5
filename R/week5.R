8c192dce9109e38313bdcb0b6685719895ab89fd
Saron M Demeke <demek004@umn.edu>
2023-02-15 23:01:22 -0600

    finished through line 18

--- R/week5.R
+++ R/week5.R
@@ -8,3 +8,11 @@ Adata_tbl <- read_delim("../data/Aparticipants.dat", delim="-",col_names = c("ca
 Anotes_tbl <- read_csv("../data/Anotes.csv")
 Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum","parnum","stimver","datadate","q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"))
 Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
+
+#Data Cleaning
+Aclean_tbl <- Adata_tbl %>% 
+  separate(qs, into = c("q1","q2","q3","q4","q5"), sep = " - ") %>% 
+  mutate(datadate = mdy_hms(datadate)) %>% 
+  mutate(across(q1:q5, as.integer)) %>% 
+  inner_join(Anotes_tbl, by="parnum") %>% 
+  filter(is.na(notes))
