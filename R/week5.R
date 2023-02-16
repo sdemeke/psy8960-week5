b3d5f655c65623409dc6e59e8cc8ac7f196b9d33
Saron M Demeke <demek004@umn.edu>
2023-02-15 22:47:24 -0600

    data import complete

--- R/week5.R
+++ R/week5.R
@@ -2,3 +2,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(lubridate)
+
+#Data Import
+Adata_tbl <- read_delim("../data/Aparticipants.dat", delim="-",col_names = c("casenum","parnum","stimver","datadate","qs"))
+Anotes_tbl <- read_csv("../data/Anotes.csv")
+Bdata_tbl <- read_tsv("../data/Bparticipants.dat", col_names = c("casenum","parnum","stimver","datadate","q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"))
+Bnotes_tbl <- read_tsv("../data/Bnotes.txt")
